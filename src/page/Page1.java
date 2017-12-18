28020ef638b32949965ff8fe8451dc814712db3d
shenruiqin <shenruiqin@ecaray.com>
2017-12-19 01:35:58 +0800

    init

---
+++ src/page/Page1.java
@@ -0,0 +1,9 @@
+package page;
+
+import utils.OutUtils;
+
+public class Page1 {
+    public static void main(String[] args) {
+        OutUtils.out("test");
+    }
+}
---
+++ src/utils/OutUtils.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class OutUtils {
+    public static void out(String out) {
+        System.out.println(out);
+    }
+}
