24f874792606d4834dc5758b09ed8c2a916a93d1
shenruiqin <shenruiqin@ecaray.com>
2017-12-19 03:30:56 +0800

    commit

--- src/page/Page1.java
+++ src/page/Page1.java
@@ -2,8 +2,43 @@ package page;
 
 import utils.OutUtils;
 
+/**
+ * 计算最大公约数
+ */
 public class Page1 {
     public static void main(String[] args) {
-        OutUtils.out("test");
+//        OutUtils.out("test");
+        gcm1(24, 60);
+    }
+
+    //统计执行的次数
+    static int cout = 0;
+
+    /**
+     * 计算最大公约数
+     */
+    public static void gcm1(int a, int b) {
+        //a<b数字交换位置，不用第三个变量
+        if (a < b) {
+            //利用+
+//            a = a + b;
+//            b = a - b;
+//            a = a - b;
+
+//            利用*
+            a = a * b;
+            b = a / b;
+            a = a / b;
+        }
+
+        //辗转相除法
+        int mod = a % b;
+        while (mod != 0) {
+            a = b;
+            b = mod;
+            mod = a % b;
+            cout++;
+        }
+        OutUtils.out(b + ", 次数: " + cout);
     }
 }
